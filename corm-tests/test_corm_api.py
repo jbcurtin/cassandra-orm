4674d243840d5a53204d05cd9bc8b13dfaa6248a
Joe Curtin <42@jbcurtin.io>
2020-09-26 07:46:53 -0400

    Added Alter Table Logic

--- corm-tests/test_corm_api.py
+++ corm-tests/test_corm_api.py
@@ -82,3 +82,45 @@ def test_select_api():
         pass
 
     assert idx > 0
+
+def test_alter_table_api():
+    from corm import register_table, insert, sync_schema, select, obtain_session
+    from corm.models import CORMBase
+    from datetime import datetime
+
+    # Create Table or Delete Column on existing Table
+    class TestModelAlter(CORMBase):
+        __keyspace__ = 'mykeyspace'
+
+        random_number: int
+        created: datetime
+
+    register_table(TestModelAlter)
+    sync_schema()
+
+    COL_CQL = f'''
+SELECT
+    column_name, type
+FROM
+    system_schema.columns
+WHERE
+    table_name = '{TestModelAlter._corm_details.table_name}'
+AND
+    keyspace_name = '{TestModelAlter._corm_details.keyspace}'
+'''
+    rows = [(row.column_name, row.type) for row in obtain_session('mykeyspace').execute(COL_CQL)]
+    assert len(rows) == 3
+
+    # Add Column on existing Table
+    class TestModelAlter(CORMBase):
+        __keyspace__ = 'mykeyspace'
+
+        random_number: int
+        created: datetime
+        new_column: str
+
+    register_table(TestModelAlter)
+    sync_schema()
+
+    rows = [(row.column_name, row.type) for row in obtain_session('mykeyspace').execute(COL_CQL)]
+    assert len(rows) == 4
--- corm/__init__.py
+++ corm/__init__.py
@@ -1,3 +1,4 @@
+import logging
 import typing
 
 from corm.constants import CLUSTER_IPS, CLUSTER_PORT, PWN
@@ -11,6 +12,8 @@ from cassandra.query import BatchStatement, SimpleStatement
 TABLES = {}
 SESSIONS = {}
 CLUSTER = Cluster(CLUSTER_IPS, port=CLUSTER_PORT)
+
+logger = logging.getLogger(__name__)
 
 def obtain_session(keyspace: str):
     if keyspace in SESSIONS.keys():
@@ -46,17 +49,67 @@ def register_table(table: typing.NamedTuple) -> None:
     table._corm_details = corm_details
 
 def sync_schema() -> None:
-    cmds_by_keyspace = {}
+    """
+    https://docs.datastax.com/en/dse/5.1/cql/cql/cql_using/useQuerySystemTable.html
+    """
+    keyspace_tables = {}
     for table_name, table in TABLES.items():
-        cmds = cmds_by_keyspace.get(table.keyspace, [])
-        cmds.append(table.as_create_keyspace_cql())
-        cmds.append(table.as_create_table_cql())
-        cmds_by_keyspace[table.keyspace] = cmds
+        tables = keyspace_tables.get(table.keyspace, [])
+        tables.append(table)
+        keyspace_tables[table.keyspace] = tables
 
-    for keyspace, cmds in cmds_by_keyspace.items():
-        session = obtain_session(keyspace)
-        for cmd in cmds:
-            session.execute(cmd)
+    for keyspace_name, tables in keyspace_tables.items():
+        obtain_session(keyspace_name).execute(table.as_create_keyspace_cql())
+        session = obtain_session(keyspace_name)
+        for table in tables:
+            COLUMN_CQL = f'''
+            SELECT
+                column_name, type
+            FROM
+                system_schema.columns
+            WHERE table_name = ?
+                AND keyspace_name = ?'''
+            stmt = obtain_session(keyspace_name).prepare(COLUMN_CQL)
+            existing_columns = {r.column_name: r.type for r in obtain_session(keyspace_name).execute(stmt, [table.table_name, keyspace_name])}
+            # Add Whole Table
+            if len(existing_columns.keys()) == 0:
+                logger.info(f'Creating Table[{table.table_name}]')
+                obtain_session(keyspace_name).execute(table.as_create_table_cql())
+
+            # Add Columns
+            elif len(table.field_names) > len(existing_columns.keys()) - 1:
+                column_updates = {}
+                for field_idx, field_name in enumerate(table.field_names):
+                    field_transliterator = table.field_transliterators[field_idx]
+                    if field_name in existing_columns.keys():
+                        if field_transliterator.cql_type.lower() != existing_columns[field_name].lower():
+                            raise NotImplementedError
+
+                    else:
+                        column_updates[field_name] = field_transliterator.cql_type
+
+                formatted_column_names = ', '.join(sorted(column_updates.keys()))
+                formatted_column_definitions = ',\n'.join([' '.join([c_name, c_type]) for c_name, c_type in column_updates.items()])
+                logger.info(f'Altering Table[{table.table_name}]. Adding Columns[{formatted_column_names}]')
+                ALTER_CQL = f'''
+ALTER TABLE
+    {keyspace_name}.{table.table_name}
+ADD ({formatted_column_definitions})
+'''
+                obtain_session(keyspace_name).execute(ALTER_CQL)
+
+            # Delete Columns
+            elif len(table.field_names) < len(existing_columns.keys()) - 1:
+                columns_to_be_removed = [key for key in existing_columns.keys() if not key in table.field_names and key != 'guid']
+                formatted_column_names = ', '.join(sorted(columns_to_be_removed))
+                ALTER_CQL = f'''
+ALTER TABLE
+    {keyspace_name}.{table.table_name}
+DROP ({formatted_column_names})
+'''
+                obtain_session(keyspace_name).execute(ALTER_CQL)
+                import pdb; pdb.set_trace()
+                pass
 
 def insert(corm_objects: typing.List[typing.Any]) -> None:
     keyspace = corm_objects[0]._corm_details.keyspace
